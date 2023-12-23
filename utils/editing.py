eaa61fe0dfa26c88b8c8b5d23f5260cf36775a86
aakashks <114357454+aakashks@users.noreply.github.com>
2023-12-23 12:35:00 +0530

    increase mask borders. good results

--- utils/editing.py
+++ utils/editing.py
@@ -1,15 +1,17 @@
 from simple_lama_inpainting import SimpleLama
-from PIL import Image
+from PIL import Image, ImageFilter
 import numpy as np
 
 
-def inpaint_area(image, mask):
+def inpaint_area(image, mask, blur_radius=5):
     """
     Inpaints the given image with the given mask.
     Must ensure that the image and mask are PIL images.
+    blur_radius is the radius of the Gaussian blur that is applied to the mask.
+    this is done to make the mask borders larger and to avoid the object getting inpainted.
     """
     simple_lama = SimpleLama()
-    mask = mask.convert('L')
+    mask = mask.convert('L').filter(ImageFilter.GaussianBlur(radius=blur_radius))
 
     # make sure the mask is binary
     mask = np.array(mask)
